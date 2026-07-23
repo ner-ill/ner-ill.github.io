fc8846d6374c3aababf2227a0a10422a1195a095
ner-ill <mumimo5656@gmail.com>
2026-07-23 12:59:17 +0900

    gallery.js を更新

--- data/gallery.js
+++ data/gallery.js
@@ -1,12 +1,12 @@
 /* category はフィルター用。必要に応じて tags: [] を追加できます。 */
 window.galleryData = [
-  { title: '緋鶴', date: '2026-07-01', category: 'original', thumbnail: 'images/gallery/oc緋鶴.webp', full: 'images/gallery/oc緋鶴.webp', alt: '', description: 'oc 朱雀のoc' },
-  { title: 'ミニキャラFA', date: '2026-06-18', category: 'fanart', thumbnail: 'images/gallery/ミニキャラFA.webp', full: 'images/gallery/ミニキャラFA.webp', alt: '', description: 'ミニキャラFA Vtuber　花狩くれは様' },
-  { title: '無償企画', date: '2026-06-03', category: 'commission', thumbnail: 'images/gallery/無償企画.webp', full: 'images/gallery/無償企画.webp', alt: '', description: 'Xにて募集した無償企画イラスト IRIAMライバーの枝葉ルクリア様' },
-  { title: '無償企画02', date: '2026-05-20', category: 'commission', thumbnail: 'images/gallery/無償企画02.webp', full: 'images/gallery/無償企画02.webp', alt: '', description: 'Xにて募集した無償企画イラスト IRIAMライバーの白骨紅様' },
-  { title: 'ミニキャラFA02', date: '2026-05-04', category: 'fanart', thumbnail: 'images/gallery/ミニキャラFA02.webp', full: 'images/gallery/ミニキャラFA02.webp', alt: '', description: 'ミニキャラFA' },
-  { title: '翠の森', date: '2026-04-12', category: 'fanart', thumbnail: 'images/gallery/翠の森.webp', full: 'images/gallery/翠の森.webp', alt: '', description: 'ローゼンメイデンFA 翠星石' },
-  { title: '花嫁', date: '2026-04-12', category: 'fanart', thumbnail: 'images/gallery/花嫁.webp', full: 'images/gallery/花嫁.webp', alt: '', description: '蟲の国の婚礼 Vtuber FA 花狩くれは様' },
+  { title: '緋鶴', date: '2026-07-23', category: 'original', thumbnail: 'images/gallery/oc緋鶴.webp', full: 'images/gallery/oc緋鶴.webp', alt: '', description: 'oc 朱雀のoc' },
+  { title: 'ミニキャラFA', date: '2026-07-23', category: 'fanart', thumbnail: 'images/gallery/ミニキャラFA.webp', full: 'images/gallery/ミニキャラFA.webp', alt: '', description: 'ミニキャラFA Vtuber　花狩くれは様' },
+  { title: '無償企画', date: '2026-07-23', category: 'commission', thumbnail: 'images/gallery/無償企画.webp', full: 'images/gallery/無償企画.webp', alt: '', description: 'Xにて募集した無償企画イラスト IRIAMライバーの枝葉ルクリア様' },
+  { title: '無償企画02', date: '2026-07-23', category: 'commission', thumbnail: 'images/gallery/無償企画02.webp', full: 'images/gallery/無償企画02.webp', alt: '', description: 'Xにて募集した無償企画イラスト IRIAMライバーの白骨紅様' },
+  { title: 'ミニキャラFA02', date: '2026-07-23', category: 'fanart', thumbnail: 'images/gallery/ミニキャラFA02.webp', full: 'images/gallery/ミニキャラFA02.webp', alt: '', description: 'ミニキャラFA' },
+  { title: '翠の森', date: '2026-07-23', category: 'fanart', thumbnail: 'images/gallery/翠の森.webp', full: 'images/gallery/翠の森.webp', alt: '', description: 'ローゼンメイデンFA 翠星石' },
+  { title: '花嫁', date: '2026-07-23', category: 'fanart', thumbnail: 'images/gallery/花嫁.webp', full: 'images/gallery/花嫁.webp', alt: '', description: '蟲の国の婚礼 Vtuber FA 花狩くれは様' },
 
 ];
 
