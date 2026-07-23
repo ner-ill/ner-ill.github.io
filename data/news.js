70ca2b7cf1564f0726a37593101d6533e7011db0
ner-ill <mumimo5656@gmail.com>
2026-07-23 12:57:50 +0900

    news.js を更新

--- data/news.js
+++ data/news.js
@@ -1,8 +1,8 @@
 /* 新着順に追加してください。url は外部記事にも変更できます。 */
 window.newsData = [
-  { date: '2026-07-19', category: 'UPDATE', title: 'ゲーム「鏡の中のEuphoria」特設ページを公開しました。', url: '../games/euphoria.html' },
-  { date: '2026-07-19', category: 'GALLERY', title: 'Gallery に新しい絵を3点追加しました。', url: '../gallery/index.html' },
-  { date: '2026-07-19', category: 'INFO', title: '兎屋を開設しました。', url: '../news/index.html' },
+  { date: '2026-07-23', category: 'UPDATE', title: 'ゲーム「鏡の中のEuphoria」特設ページを公開しました。', url: '../games/euphoria.html' },
+  { date: '2026-07-23', category: 'GALLERY', title: 'Gallery に新しい絵を追加しました。', url: '../gallery/index.html' },
+  { date: '2026-07-23', category: 'INFO', title: '兎屋を開設しました。', url: '../news/index.html' },
 
 ];
 
